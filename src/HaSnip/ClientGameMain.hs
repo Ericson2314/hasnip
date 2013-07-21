ca61fff82ff91d3fcd2b052b7f5f94bbf782a064
John Ericson <Ericson2314@Yahoo.com>
2013-07-21 11:41:35 -0400

    tweaked ClientGameMain

--- src/HaSnip/ClientGameMain.hs
+++ src/HaSnip/ClientGameMain.hs
@@ -8,7 +8,7 @@ import qualified Data.ByteString.Char8 as CB
 
 import Data.Word
 
-import Network.Socket(SockAddr)
+import Network.Socket(SockAddr(SockAddrInet, SockAddrInet6))
 import Network.Socket.ByteString
 
 import Network.ENet
@@ -16,27 +16,32 @@ import qualified Network.ENet.Bindings as B
 import qualified Network.ENet.Host as Host
 import qualified Network.ENet.Peer as Peer
 
+-- localhost w/ default port is: SockAddr 32887 16777343
 
 init :: SockAddr -> IO ()
 init servAddr = withENetDo $ do
   client <- Host.create
             Nothing -- create a client host
             1       -- only allow 1 outgoing connection
-            2       -- allow up 2 channels to be used, 0 and 1
+            1       -- allow only 1 channel to be used, 0
             0       -- unlimited bandwidth in
             0       -- unlimited bandwidth out
 
-  host <- Host.connect client servAddr 1 3 -- 3 is AoS 0.75 magic number
+  host <- Host.connect
+          client   -- us
+          servAddr -- server address
+          1        -- use 1 channel
+          3        -- 3 is AoS 0.75 magic number
 
-  let loop = do temp <- Host.service client 100
+  let loop = do temp <- Host.service client 750
                 case temp of
-                  Nothing -> return ()
                   (Just (B.Event t p chan dat pack)) -> case t of
-                    B.None       -> print "no news"
-                    B.Connect    -> print "incomming!!!!"
-                    B.Disconnect -> print "payback!!!!"
-                    B.Receive    -> print "news!!!!"
-                loop
+                    B.None       -> print "got none"       >> loop
+                    B.Connect    -> print "got connect"    >> loop
+                    B.Disconnect -> print "got disconnect" >> return ()
+                    B.Receive    -> print "got receive"    >> loop
+                  Nothing      ->   print "no packet"    >>   loop
+
   loop
 
   return ()
